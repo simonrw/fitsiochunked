fbdd95d27642f8d3a07e808ca8ea2c489bef439a
Simon Walker <s.r.walker101@googlemail.com>
2016-02-11 12:01:45 +0000

    Add hdu fetching

--- fitsioyielder/fitsioyielder.py
+++ fitsioyielder/fitsioyielder.py
@@ -7,4 +7,11 @@ def open(filename):
     yield Yielder()
 
 class Yielder(object):
+    def __init__(self):
+        self.fitsio = None
+
+    def __getitem__(self, key):
+        return HDU()
+
+class HDU(object):
     pass
--- tests/test_fitsioyielder.py
+++ tests/test_fitsioyielder.py
@@ -19,4 +19,19 @@ def test_open(filename):
     with fitsioyielder.open(filename) as infile:
         assert isinstance(infile, fitsioyielder.Yielder)
 
+def test_yielder_has_fitsio_object(filename):
+    with fitsioyielder.open(filename) as infile:
+        assert hasattr(infile, 'fitsio')
 
+def test_can_get_hdu(filename):
+    with fitsioyielder.open(filename) as infile:
+        assert isinstance(infile['flux'], fitsioyielder.HDU)
+
+
+'''
+Expected API
+
+with fitsioyielder.open(filename) as infile:
+    for chunk in infile['flux'].chunks(chunksize=10):
+        # do something with chunk
+'''
