112a4284be6096ef15bb98258498c1cb00cf0ff9
Simon Walker <s.r.walker101@googlemail.com>
2016-03-15 18:22:24 +0000

    Release v0.0.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='fitsiochunked',
-    version='0.0.3',
+    version='0.0.4',
     description="Package to efficiently read large fits arrays in object by object",
     long_description=readme,
     author="Simon Walker",
